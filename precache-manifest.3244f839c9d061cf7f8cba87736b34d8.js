49f3100570c4276f704cea0faca7ef80dd54c77f
Jake Morrison <blakemorrison727@gmail.com>
2020-03-15 14:58:26 -0700

    Updates

---
+++ precache-manifest.3244f839c9d061cf7f8cba87736b34d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5140b50a75f4729ab6d5",
+    "url": "/lightsout/static/js/runtime~main.5140b50a.js"
+  },
+  {
+    "revision": "c90b0d5059301c0d7859",
+    "url": "/lightsout/static/js/main.c90b0d50.chunk.js"
+  },
+  {
+    "revision": "cd22fa7af0c3eaf7956e",
+    "url": "/lightsout/static/js/1.cd22fa7a.chunk.js"
+  },
+  {
+    "revision": "c90b0d5059301c0d7859",
+    "url": "/lightsout/static/css/main.3a9759cb.chunk.css"
+  },
+  {
+    "revision": "cbd5d35a78a07b26ca120947b790a02a",
+    "url": "/lightsout/index.html"
+  }
+];
